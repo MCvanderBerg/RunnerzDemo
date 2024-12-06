2c34b8e86b75b28ffc02a4a3e836c92ad1863b6a
MCvanderBerg <christiaan26c@gmail.com>
2024-12-06 14:18:51 +0200

    first commit

---
+++ src/test/java/dev/christiaan/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.christiaan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
